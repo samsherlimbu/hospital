ed13829101eaee20085d4f3af0e209127882a2e8
samsherlimbu <samshershigu6@gmail.com>
2024-05-23 22:15:56 +0545

    updating image slider

--- client/src/app/components/imageSlider/page.js
+++ client/src/app/components/imageSlider/page.js
@@ -1,45 +1,52 @@
 'use client'
+import React from 'react'
+import { useState, useEffect } from 'react';
 
-import Image from 'next/image'
-import { images } from '../../../../lib/images'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import { Navigation, Pagination } from 'swiper/modules'
+const images = [
+  "/first.png",
+  "/second.png",
+  "/third.png"
+];
+const ImageSlider=()=> {
+  const [currentSlide, setCurrentSlide] = useState(0);
 
+  useEffect(() => {
+    const slideInterval = setInterval(() => {
+      setCurrentSlide((prevSlide) => (prevSlide + 1) % images.length);
+    }, 5000);
+    return () => clearInterval(slideInterval);
+  }, []);
 
-import 'swiper/css'
-import 'swiper/css/navigation'
-import 'swiper/css/pagination'
+  const nextSlide = () => {
+    setCurrentSlide((currentSlide + 1) % images.length);
+  };
 
+  const prevSlide = () => {
+    setCurrentSlide((currentSlide - 1 + images.length) % images.length);
+  };
 
-
-const Slider = ()=> {
   return (
-    <>
-        <Swiper
-          navigation
-          pagination={{ type: 'fraction' }}
-          modules={[Navigation, Pagination]}
-          onSwiper={swiper => console.log(swiper)}
-          className='h-96 w-full rounded-lg'
-        >
-          {images.map((image, index) => (
-            <SwiperSlide key={index}>
-              <div className='flex h-full w-full items-center justify-center'>
-                <Image
-                  src={image.src}
-                  alt={image.alt}
-                  layout='responsive'
-                  width={1920} 
-                  height={1080} 
-                  className='w-full h-full object-cover'
-                />
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      
-    </>
-  )
+    <div className="relative w-full h-80 overflow-hidden">
+      <div className="flex transition-transform ease-in-out duration-500" style={{ transform: `translateX(-${currentSlide * 100}%)` }}>
+        {images.map((src, index) => (
+          <div key={index} className="min-w-full h-80 relative">
+            <img src={src} alt={`Slide ${index}`} className="w-full h-full object-cover" />
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center bg-gray-500 bg-opacity-50 p-4 rounded">
+              <h2 className="text-2xl font-bold">KANTIPUR</h2>
+              <p className="text-lg">International Hospital</p>
+              <p className="text-sm">A hospital may be a place of illness, but it's also a place of healing, hope, and humanity</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <button onClick={prevSlide} className="absolute top-1/2 left-0 transform -translate-y-1/2 p-4 text-white bg-black bg-opacity-50 rounded-r">
+        &#10094;
+      </button>
+      <button onClick={nextSlide} className="absolute top-1/2 right-0 transform -translate-y-1/2 p-4 text-white bg-black bg-opacity-50 rounded-l">
+        &#10095;
+      </button>
+    </div>
+  );
 }
 
-export default Slider;
+export default ImageSlider;
