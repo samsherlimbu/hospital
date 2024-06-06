9ddf0373a3060e51211a34f54484668edd6850e4
samsherlimbu <samshershigu6@gmail.com>
2024-06-06 08:57:17 +0545

    jwt in serverside

--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const saltRounds = 10;
 const dbConnect = require('./src/db/connection')
 
 
@@ -28,12 +31,21 @@ const port = process.env.PORT
 
 
 app.post('/register',async (req, res) => {
+  const hashPassword = await bcrypt.hash(req.body.password,saltRounds)
+  console.log(hashPassword);
+  req.body.password =  hashPassword;
+
+
   console.log(req.body.phoneNumber)
   console.log(req.body)
   const phoneuserExists = await User.exists({phoneNumber:req.body.phoneNumber})
   const emailuserExists = await User.exists({email:req.body.email})
+
+
   console.log(phoneuserExists);
   console.log(emailuserExists);
+
+
   if(phoneuserExists){
    return res.send({message:'PhoneNumber already exists'})
   }else if(emailuserExists){
@@ -46,6 +58,25 @@ app.post('/register',async (req, res) => {
 app.get('/register', async (req, res) => {
   const data = await User.find()
   res.send(data)
+})
+
+app.post('/login', async (req, res) => {
+  console.log(req.body);
+  const user = await User.findOne({phoneNumber:req.body.phoneNumber})
+  if (user) {
+    const match = await bcrypt.compare(req.body.password, user.password);
+    if (match) {
+      const token = jwt.sign({ phoneNumber: req.body.phoneNumber }, process.env.SECRET_KEY);//secret key from the .env
+      console.log(token);
+
+
+      return res.send({ message: 'authorized', token });
+    } else {
+      return res.send({ message: 'Invalid password' });
+    }
+  }else{
+    res.json({ message: 'phoneNUmber not registered' });
+  }
 })
 
 app.listen(port, () => {
