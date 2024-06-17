b124f659896461923c78faa8610c0f9b01f66499
samsherlimbu <samshershigu6@gmail.com>
2024-06-17 14:01:11 +0545

    updated addpatient

--- client/src/app/pages/addpatient/page.js
+++ client/src/app/pages/addpatient/page.js
@@ -1,58 +1,83 @@
-'use client';
+"use client";
 import React from "react";
 import Link from "next/link";
 import { useFormik } from "formik";
 import * as Yup from "yup";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 const genders = ["Male", "Female", "Other"];
 const bloodGroups = ["A+", "B+", "O+", "AB+", "A-", "B-", "O-", "AB-"];
-const status =["Active", "Inactive"]
+const statusOptions = ["Active", "Inactive"];
 
 const AddPatientForm = ({ onCancel }) => {
+  const router = useRouter();
 
   const registerSchema = Yup.object().shape({
-    name: Yup.string()
-      .min(3, 'Too Short!')
-      .max(50, 'Too Long!')
-      .required('Required'),
-    email: Yup.string().email('Invalid email').required('Required'),
+    fullName: Yup.string()
+      .min(3, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
+    email: Yup.string().email("Invalid email").required("Required"),
     password: Yup.string()
-      .min(8, 'Password must be at least 8 characters')
-      .required('Required'),
+      .min(8, "Password must be at least 8 characters")
+      .required("Required"),
     confirmPassword: Yup.string()
-      .oneOf([Yup.ref('password'), null], 'Passwords must match')
-      .required('Required'),
-    phone: Yup.string()
-      .matches(/^[0-9]+$/, "Phone number must be numeric")
-      .min(10, 'Phone number must be at least 10 digits')
-      .required('Required'),
-    gender: Yup.string().oneOf(genders).required('Required'),
-    bloodGroup: Yup.string().oneOf(bloodGroups).required('Required'),
-    //status: Yup.string().oneOf(statusOptions).required('Required'),
+      .oneOf([Yup.ref("password"), null], "Passwords must match")
+      .required("Required"),
+    phoneNumber: Yup.string()
+      .matches(/^[0-9]+$/, "phoneNumber number must be numeric")
+      .min(10, "phoneNumber number must be at least 10 digits")
+      .required("Required"),
+    gender: Yup.string().oneOf(genders).required("Required"),
+    bloodGroup: Yup.string().oneOf(bloodGroups).required("Required"),
+    status: Yup.string().oneOf(statusOptions).required("Required"),
     address: Yup.string(),
-    date: Yup.date().required('Required'),
-    terms: Yup.boolean().oneOf([true], 'You must accept the terms and conditions')
+    date: Yup.date().required("Required"),
+    terms: Yup.boolean().oneOf(
+      [true],
+      "You must accept the terms and conditions"
+    ),
   });
 
   const formik = useFormik({
     initialValues: {
-      name: '',
-      email: '',
-      password: '',
-      confirmPassword: '',
-      phone: '',
-      gender: '',
-      bloodGroup: '',
-      status: '',
-      address: '',
-      date: '',
-      terms: false
+      fullName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      phoneNumber: "",
+      gender: "",
+      bloodGroup: "",
+      status: "",
+      address: "",
+      date: "",
+      terms: false,
     },
     validationSchema: registerSchema,
-    onSubmit: values => {
-      alert(JSON.stringify(values, null, 2));
-    }
+    onSubmit: (values) => {
+      registerPatient(values);
+    },
   });
+
+  const registerPatient = async (values) => {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(values),
+    };
+    const response = await fetch(
+      "http://localhost:8000/register",
+      requestOptions
+    );
+    const data = await response.json();
+    if (response.status == "200") {
+      toast.success(data.message);
+      router.push("/pages/patient");
+    } else {
+      toast.error(data.message);
+    }
+  };
 
   return (
     <div className=" flex items-center justify-center bg-gray-100 w-full h-full mt-0">
@@ -65,16 +90,18 @@ const AddPatientForm = ({ onCancel }) => {
             </label>
             <input
               type="text"
-              id="name"
-              name="name"
+              id="fullName"
+              name="fullName"
               className="mt-1 block w-full pl-3 pr-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
               placeholder="Name"
-              value={formik.values.name}
+              value={formik.values.fullName}
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
             />
-            {formik.touched.name && formik.errors.name ? (
-              <div className="text-red-500 text-sm">{formik.errors.name}</div>
+            {formik.touched.fullName && formik.errors.fullName ? (
+              <div className="text-red-500 text-sm">
+                {formik.errors.fullName}
+              </div>
             ) : null}
           </div>
 
@@ -94,10 +121,12 @@ const AddPatientForm = ({ onCancel }) => {
                 onBlur={formik.handleBlur}
               />
               {formik.touched.email && formik.errors.email ? (
-                <div className="text-red-500 text-sm">{formik.errors.email}</div>
+                <div className="text-red-500 text-sm">
+                  {formik.errors.email}
+                </div>
               ) : null}
             </div>
-            
+
             <div>
               <label className="block text-sm font-medium text-gray-700">
                 Password <span className="text-red-500">*</span>
@@ -113,7 +142,9 @@ const AddPatientForm = ({ onCancel }) => {
                 onBlur={formik.handleBlur}
               />
               {formik.touched.password && formik.errors.password ? (
-                <div className="text-red-500 text-sm">{formik.errors.password}</div>
+                <div className="text-red-500 text-sm">
+                  {formik.errors.password}
+                </div>
               ) : null}
             </div>
           </div>
@@ -133,26 +164,30 @@ const AddPatientForm = ({ onCancel }) => {
               onBlur={formik.handleBlur}
             />
             {formik.touched.confirmPassword && formik.errors.confirmPassword ? (
-              <div className="text-red-500 text-sm">{formik.errors.confirmPassword}</div>
+              <div className="text-red-500 text-sm">
+                {formik.errors.confirmPassword}
+              </div>
             ) : null}
           </div>
 
           <div>
             <label className="block text-sm font-medium text-gray-700">
-              Phone <span className="text-red-500">*</span>
+              phoneNumber <span className="text-red-500">*</span>
             </label>
             <input
               type="tel"
-              id="phone"
-              name="phone"
+              id="phoneNumber"
+              name="phoneNumber"
               className="mt-1 block w-full pl-3 pr-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-              placeholder="Phone Number"
-              value={formik.values.phone}
+              placeholder="phoneNumber"
+              value={formik.values.phoneNumber}
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
             />
-            {formik.touched.phone && formik.errors.phone ? (
-              <div className="text-red-500 text-sm">{formik.errors.phone}</div>
+            {formik.touched.phoneNumber && formik.errors.phoneNumber ? (
+              <div className="text-red-500 text-sm">
+                {formik.errors.phoneNumber}
+              </div>
             ) : null}
           </div>
 
@@ -177,7 +212,9 @@ const AddPatientForm = ({ onCancel }) => {
                 ))}
               </select>
               {formik.touched.gender && formik.errors.gender ? (
-                <div className="text-red-500 text-sm">{formik.errors.gender}</div>
+                <div className="text-red-500 text-sm">
+                  {formik.errors.gender}
+                </div>
               ) : null}
             </div>
 
@@ -201,7 +238,9 @@ const AddPatientForm = ({ onCancel }) => {
                 ))}
               </select>
               {formik.touched.bloodGroup && formik.errors.bloodGroup ? (
-                <div className="text-red-500 text-sm">{formik.errors.bloodGroup}</div>
+                <div className="text-red-500 text-sm">
+                  {formik.errors.bloodGroup}
+                </div>
               ) : null}
             </div>
           </div>
@@ -221,36 +260,10 @@ const AddPatientForm = ({ onCancel }) => {
               onBlur={formik.handleBlur}
             ></textarea>
             {formik.touched.address && formik.errors.address ? (
-              <div className="text-red-500 text-sm">{formik.errors.address}</div>
-            ) : null}
-          </div>
-          <div>
-            <label className="block text-sm font-medium text-gray-700">
-              Photo
-            </label>
-            <div className="mt-1 flex justify-center border-2 border-dashed border-black rounded-md py-6 px-6">
-              <div className="text-center">
-                <svg
-                  className="mx-auto h-12 w-12 text-gray-400"
-                  stroke="currentColor"
-                  fill="none"
-                  viewBox="0 0 48 48"
-                  aria-hidden="true"
-                >
-                  <path d="M24 0v48M0 24h48" />
-                </svg>
-                <div className="flex text-sm text-gray-600 mt-2">
-                  <label
-                    className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
-                  >
-                    <span>Upload a file</span>
-                    <input id="file-upload" name="file-upload" type="file" className="sr-only" />
-                  </label>
-                  <p className="pl-1">or drag and drop</p>
-                </div>
-                <p className="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
+              <div className="text-red-500 text-sm">
+                {formik.errors.address}
               </div>
-            </div>
+            ) : null}
           </div>
 
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
@@ -285,17 +298,57 @@ const AddPatientForm = ({ onCancel }) => {
                 onBlur={formik.handleBlur}
               >
                 <option value="" label="Select status" />
-                {status.map((item, index) => (
+                {statusOptions.map((item, index) => (
                   <option value={item} key={index}>
                     {item}
                   </option>
                 ))}
               </select>
               {formik.touched.status && formik.errors.status ? (
-                <div className="text-red-500 text-sm">{formik.errors.status}</div>
+                <div className="text-red-500 text-sm">
+                  {formik.errors.status}
+                </div>
               ) : null}
             </div>
           </div>
+
+          <div>
+            <label className="block text-sm font-medium text-gray-700">
+              Photo
+            </label>
+            <div className="mt-1 flex justify-center border-2 border-dashed border-gray-300 rounded-md py-6 px-6">
+              <div className="text-center">
+                <svg
+                  className="mx-auto h-12 w-12 text-gray-400"
+                  stroke="currentColor"
+                  fill="none"
+                  viewBox="0 0 48 48"
+                  aria-hidden="true"
+                >
+                  <path d="M24 0v48M0 24h48" />
+                </svg>
+                <div className="flex text-sm text-gray-600 mt-2">
+                  <label
+                    htmlFor="file-upload"
+                    className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+                  >
+                    <span>Upload a file</span>
+                    <input
+                      id="file-upload"
+                      name="file-upload"
+                      type="file"
+                      className="sr-only"
+                    />
+                  </label>
+                  <p className="pl-1">or drag and drop</p>
+                </div>
+                <p className="text-xs text-gray-500">
+                  PNG, JPG, GIF up to 10MB
+                </p>
+              </div>
+            </div>
+          </div>
+
           <div className="flex items-center">
             <input
               id="terms"
@@ -318,13 +371,20 @@ const AddPatientForm = ({ onCancel }) => {
             <div className="text-red-500 text-sm">{formik.errors.terms}</div>
           ) : null}
 
-          
-          {formik.touched.terms && formik.errors.terms ? (
-            <div className="text-red-500 text-sm">{formik.errors.terms}</div>
-          ) : null}
           <div className="flex justify-end space-x-2">
-            <button type="button" onClick={onCancel} className="bg-gray-500 text-white px-4 py-2 rounded">Cancel</button>
-            <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">Add Patient</button>
+            <button
+              type="button"
+              onClick={onCancel}
+              className="bg-gray-500 text-white px-4 py-2 rounded"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="bg-blue-500 text-white px-4 py-2 rounded"
+            >
+              Add Patient
+            </button>
           </div>
         </form>
       </div>
